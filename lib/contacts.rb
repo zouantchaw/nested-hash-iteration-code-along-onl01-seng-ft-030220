2b5b20567cf8044c06dd95b130ee0a2994a2aefe
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-15 18:37:52 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,10 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    binding.pry
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        binding.pry 
+      end 
+    end 
   end 
 end
